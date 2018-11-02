e5985fab4570609ed6b2d2440cfb22d14d7b85f9
herong <1556592626@qq.com>
2018-11-02 20:17:30 +0800

    hr重新布置,components中的pages拿到src中了

--- src/components/pages/shoppingCart/main/shopping.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        shopping
-    </div>
-</template>
--- src/pages/index/main/index.vue
+++ src/pages/index/main/index.vue
@@ -1,44 +1,37 @@
 <template>
-	<div style="margin-top:118px;">
-		<MyBanner effect='fade'>
-			<div slot="swiper-con" class="swiper-slide">
-				<img src="../../../assets/images/Banner/banner01.jpg" alt="" class="swiper-img">
-			</div>
-			<div slot="swiper-con" class="swiper-slide">
-				<img src="../../../assets/images/Banner/banner02.jpg" alt="" class="swiper-img">
-			</div>
-			<div slot="swiper-con" class="swiper-slide">
-				<img src="../../../assets/images/Banner/banner03.jpg" alt="" class="swiper-img">
-			</div>
-		</MyBanner>
-		<MyIndexNumInfo></MyIndexNumInfo>
-		<MyIndexSection></MyIndexSection>
-		<MyIndexSection id="sec2"></MyIndexSection>
-	</div>
+    <div>
+        <MyBanner effect='fade'>
+            <div slot="swiper-con" class="swiper-slide">
+                <img src="../../../../assets/images/Banner/banner01.jpg" alt="" class="swiper-img">
+            </div>
+             <div slot="swiper-con" class="swiper-slide">
+                <img src="../../../../assets/images/Banner/banner02.jpg" alt="" class="swiper-img">
+            </div>
+             <div slot="swiper-con" class="swiper-slide">
+                <img src="../../../../assets/images/Banner/banner03.jpg" alt="" class="swiper-img">
+            </div>
+        </MyBanner>
+        <MyIndexNumInfo></MyIndexNumInfo>
+        <MyIndexSection></MyIndexSection>
+        <MyIndexSection id="sec2"></MyIndexSection>
+    </div>
 </template>
 
 <script>
-	import MyBanner from '@/components/Banner'
-	import MyIndexNumInfo from '../IndexNumInfo'
-	import MyIndexSection from '../IndexSectionWrap'
-	import { getUuid } from '@/api'
-	
-	export default {
-		components: {
-			MyBanner,
-			MyIndexNumInfo,
-			MyIndexSection
-		},
-		created(){
-			getUuid().then(res=>{
-				console.log(res);
-			})
-		}
-	}
+import MyBanner from '../../../Banner'
+import MyIndexNumInfo from '../IndexNumInfo'
+import MyIndexSection from '../IndexSectionWrap'
+export default {
+    components:{
+        MyBanner,
+        MyIndexNumInfo,
+        MyIndexSection
+    }
+}
 </script>
 
 <style>
-	#sec2 {
-		background: url("../../../assets/images/section_bg_2.png") !important;
-	}
+#sec2{background:url("../../../../assets/images/section_bg_2.png")!important;}
 </style>
+
+
