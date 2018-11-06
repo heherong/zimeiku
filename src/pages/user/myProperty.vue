3e3b43114a55a9036375f03969a9179c302d108b
dongjie14 <18676361434@163.com>
2018-11-06 17:46:21 +0800

    user

--- src/pages/user/myProperty.vue
+++ src/pages/user/myProperty.vue
@@ -1,9 +1,150 @@
 <template>
-	<div></div>
+	<div class="inform">
+		<div class="myMoney">
+			<h4>我的钱包 </h4>
+			<el-row class="myMoney-cash">
+				<el-col :span="3">
+					<div class="grid-content">账户余额：</div>
+				</el-col>
+				<el-col :span="15">
+					<div class="cash">{{myMoney}}</div>
+				</el-col>
+				<el-button type="primary">提现</el-button>
+			</el-row>
+		</div>
+		<div class="moneyList">
+			<h4>账目明细 </h4>
+			<el-row class="list-status">
+				<el-col :span="6">
+					<span @click="changeStatus(1)" :class=" status==1?'active':'' ">全部</span>
+				</el-col>
+				<el-col :span="6">
+					<span @click="changeStatus(2)" :class=" status==2?'active':'' ">支出</span>
+				</el-col>
+				<el-col :span="6">
+					<span @click="changeStatus(3)" :class=" status==3?'active':'' ">收入</span>
+				</el-col>
+				<el-col :span="6">
+					<span @click="changeStatus(4)" :class=" status==4?'active':'' ">提现</span>
+				</el-col>
+			</el-row>
+			<el-table :data="tableData" style="width: 100%">
+				<el-table-column label="时间" width="200">
+					<template slot-scope="scope">
+						<span>{{ scope.row.date }}</span>
+					</template>
+				</el-table-column>
+				<el-table-column label="金额" width="150">
+					<template slot-scope="scope">
+						<span>{{ scope.row.cash }}</span>
+					</template>
+				</el-table-column>
+				<el-table-column label="类型">
+					<template slot-scope="scope" width="120">
+						<span>{{ scope.row.status }}</span>
+					</template>
+				</el-table-column>
+				<el-table-column label="相关">
+					<template slot-scope="scope">
+						<span style="overflow: hidden;text-overflow:ellipsis;white-space: nowrap;">{{ scope.row.address }}</span>
+					</template>
+				</el-table-column>
+			</el-table>
+			<div class="pages">
+				<el-pagination background layout="prev, pager, next" :total="1000">
+				</el-pagination>
+			</div>
+		</div>
+
+	</div>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				myMoney: '120.00',
+				status:1,
+				tableData: [{
+					date: '2016-05-02',
+					cash: '89',
+					status: '购买',
+					address: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄'
+				}, {
+					date: '2016-05-04',
+					cash: '12.22',
+					status: '购买',
+					address: '上海市普陀区金沙江路 1517 弄'
+				}, {
+					date: '2016-05-01',
+					cash: '23.3',
+					status: '出售',
+					address: '上海市普陀区金沙江路 1519 弄'
+				}, {
+					date: '2016-05-03',
+					cash: '42',
+					status: '购买',
+					address: '上海市普陀区金沙江路 1516 弄'
+				}, {
+					date: '2016-05-04',
+					cash: '77',
+					status: '出售',
+					address: '上海市普陀区金沙江路 1517 弄'
+				}, {
+					date: '2016-05-01',
+					cash: '34',
+					status: '出售',
+					address: '上海市普陀区金沙江路 1519 弄'
+				}, {
+					date: '2016-05-03',
+					cash: '12',
+					status: '购买',
+					address: '上海市普陀区金沙江路 1516 弄'
+				}]
+			}
+		},
+		methods: {
+			handleEdit(index, row) {
+				console.log(index, row);
+			},
+			handleDelete(index, row) {
+				console.log(index, row);
+			},
+			changeStatus:function(index){
+				let that = this;
+				that.status = index;
+			}
+		}
+	}
 </script>
 
 <style>
+	@import url("../../assets/css/user");
+	.list-status{
+		margin-bottom: 20px;
+	}
+	.list-status span{
+		display: inline-block;
+		width:50%;
+		cursor: pointer;
+		text-align: center;
+	}
+	.list-status .active{
+		padding-bottom: 5px;
+		border-bottom:solid 2px #409EFF;
+	}
+	.cash {
+		font-size: 20px;
+		color: #56BCEB;
+	}
+	
+	.myMoney-cash {
+		line-height: 60px;
+		background-color: #eef1f6;
+		padding-left: 10px;
+	}
+	
+	.myMoney {
+		margin-bottom: 50px;
+	}
 </style>
--- src/pages/user/setting.vue
+++ src/pages/user/setting.vue
@@ -1,8 +1,265 @@
 <template>
+	<div class="inform">
+		
+		<div v-if="settingShow==1">
+			<h4>基本信息 </h4>
+			<el-row>
+				<!--头像-->
+				<el-col :span="24" class="col-head">
+					<div @click="toChangeHead" class="head-image">
+						<img :src=headImg />
+						<p>点击修改头像</p>
+					</div>
+				</el-col>
+			</el-row>
+			<!--内容-->
+			<div class="info-content">
+				<!--昵称-->
+				<el-row>
+					<el-col :span="6"><span>昵称：</span></el-col>
+					<el-col :span="12" v-if="name"><span>{{name}}</span><i class="el-icon-circle-check"></i></el-col>
+					<el-col :span="12" v-else><span>未设定</span></el-col>
+
+					<el-col :span="6" v-if="name"><span class="col-change" @click="toChangeName(1)">修改</span></el-col>
+					<el-col :span="6" v-else><span class="col-change" @click="toChangeName(2)">去设定</span></el-col>
+				</el-row>
+
+				<!--认证状态-->
+				<el-row>
+					<el-col :span="6"><span>认证状态：</span></el-col>
+					<el-col :span="12" v-if="identificationStatus"><span>已认证</span><i class="el-icon-circle-check"></i></el-col>
+					<el-col :span="12" v-else><span>未认证</span></el-col>
+
+					<el-col :span="6" v-if="identificationStatus"><span class="col-change" @click="quitIdentification(1)">取消认证</span></el-col>
+					<el-col :span="6" v-else><span class="col-change" @click="quitIdentification(2)">去认证</span></el-col>
+				</el-row>
+				<!--绑定手机-->
+				<el-row>
+					<el-col :span="6"><span>绑定手机：</span></el-col>
+					<el-col :span="12" v-if="iphone"><span>{{iphone}}</span><i class="el-icon-circle-check"></i></el-col>
+					<el-col :span="12" v-else><span>未绑定</span></el-col>
+
+					<el-col :span="6" v-if="iphone"><span class="col-change" @click="toChangePhone(1)">修改</span></el-col>
+					<el-col :span="6" v-else><span class="col-change" @click="toChangePhone(2)">去绑定</span></el-col>
+				</el-row>
+
+				<el-row>
+					<el-col :span="6"><span>绑定邮箱：</span></el-col>
+					<el-col :span="12" v-if="email"><span>{{email}}</span><i class="el-icon-circle-check"></i></el-col>
+					<el-col :span="12" v-else><span>未绑定</span></el-col>
+
+					<el-col :span="6" v-if="email"><span class="col-change" @click="toChangeEmail(1)">修改</span></el-col>
+					<el-col :span="6" v-else><span class="col-change" @click="toChangeEmail(2)">去绑定</span></el-col>
+				</el-row>
+				<el-row>
+					<el-col :span="6"><span>绑定微信：</span></el-col>
+					<el-col :span="12" v-if="weixin"><span>已绑定</span><i class="el-icon-circle-check"></i></el-col>
+					<el-col :span="12" v-else><span>未绑定</span></el-col>
+
+					<el-col :span="6" v-if="weixin"><span class="col-change" @click="toChangeWeixin(1)">修改</span></el-col>
+					<el-col :span="6" v-else><span class="col-change" @click="toChangeWeixin(2)">去绑定</span></el-col>
+				</el-row>
+				<el-row>
+					<el-col :span="6"><span>登录密码：</span></el-col>
+					<el-col :span="12" v-if="loginPassword"><span>已设定</span><i class="el-icon-circle-check"></i></el-col>
+					<el-col :span="12" v-else><span>未设定</span></el-col>
+
+					<el-col :span="6" v-if="loginPassword"><span class="col-change" @click="toChangeLogin(1)">更换</span></el-col>
+					<el-col :span="6" v-else><span class="col-change" @click="toChangeLogin(2)">去设定</span></el-col>
+				</el-row>
+				<el-row>
+					<el-col :span="6"><span>支付密码：</span></el-col>
+					<el-col :span="12" v-if="payPassword"><span>已设定</span><i class="el-icon-circle-check"></i></el-col>
+					<el-col :span="12" v-else><span>未设定</span></el-col>
+
+					<el-col :span="6" v-if="payPassword"><span class="col-change" @click="toChangePay(1)">更换</span></el-col>
+					<el-col :span="6" v-else><span class="col-change" @click="toChangePay(2)">去设定</span></el-col>
+				</el-row>
+			</div>
+		</div>
+		
+		<div v-if="settingShow==3">
+			<h4>基本信息</h4>
+			<el-row>
+				<el-col :span="24" class="col-head">
+					<p style="font-size: 14px;">(提示：修改手机号需先验证已绑定的手机号)</p>
+				</el-col>
+			</el-row>
+		</div>
+	</div>
 </template>
 
 <script>
+	import jpg1 from '@/assets/images/1.jpg';
+	export default {
+		data() {
+			return {
+				settingShow:3, //显示页面 1是当前，2是认证，3是手机，4是邮箱，5是微信，6是登录密码，7是支付密码
+				headImg: jpg1,
+				name: "",
+				identificationStatus: true, //认证状态
+				iphone: 1871234123,
+				email: '1@qq.com',
+				weixin: 'sweet',
+				loginPassword: true, //登录密码
+				payPassword: true, //支付密码
+			}
+		},
+		methods: {
+			//切换头像
+			toChangeHead: function() {
+
+			},
+			//修改昵称 状态为1，为修改，其他则添加
+			toChangeName: function(index) {
+				let that = this;
+				if(index == 1) {
+					let htmlContent = `<input class=change-input id=name type=text name=name value=${that.name}>`;
+					that.$confirm(htmlContent, '修改昵称', {
+						confirmButtonText: '确定',
+						cancelButtonText: '取消',
+						center: true,
+						dangerouslyUseHTMLString: true,
+						beforeClose: (action, instance, done) => {
+							if(action === 'confirm') {
+
+								if(document.getElementById('name').value != '') {
+									that.name = document.getElementById('name').value;
+									done();
+									that.$message({
+										message: '修改成功',
+										type: 'success'
+									});
+								} else {
+									that.$message('内容不能为空');
+								}
+
+							} else {
+								done();
+							}
+						}
+					}).then(({
+						value
+					}) => {}).catch(() => {});
+
+				} else {
+					let htmlContent = `<input class=change-input id=name type=text name=name >`;
+					that.$confirm(htmlContent, '设置昵称', {
+						confirmButtonText: '确定',
+						cancelButtonText: '取消',
+						center: true,
+						dangerouslyUseHTMLString: true,
+						beforeClose: (action, instance, done) => {
+							if(action === 'confirm') {
+
+								if(document.getElementById('name').value != '') {
+									that.name = document.getElementById('name').value;
+									done();
+									that.$message({
+										message: '设置成功',
+										type: 'success'
+									});
+								} else {
+									that.$message('内容不能为空');
+								}
+
+							} else {
+								done();
+							}
+						}
+					}).then(({
+						value
+					}) => {}).catch(() => {});
+				}
+
+			},
+			//认证
+			quitIdentification: function() {
+
+			},
+			//手机
+			toChangePhone: function(index) {
+				let that = this;
+				if(index == 1) {
+
+				} else {
+
+				}
+			},
+			//邮箱
+			toChangeEmail: function() {
+
+			},
+			//微信
+			toChangeWeixin: function() {
+
+			},
+			//登录密码
+			toChangeLogin: function() {
+
+			},
+			//支付密码
+			toChangePay: function() {
+
+			}
+		}
+	}
 </script>
 
 <style>
+	@import url("../../assets/css/user");
+	.head-image {
+		cursor: pointer;
+	}
+	
+	.head-image>img {
+		width: 70px;
+		height: 70px;
+		border-radius: 70px;
+	}
+	
+	.head-image>p {
+		color: #409EFF;
+		font-size: 14px;
+		margin-top: 5px;
+	}
+	
+	.col-head {
+		text-align: center;
+	}
+	
+	.info-content {
+		text-align: center;
+		margin-top: 30px;
+	}
+	
+	.info-content span {
+		text-align: center;
+		color: #606266;
+		font-size: 14px;
+	}
+	
+	.info-content .el-row {
+		padding: 30px 0 10px 0;
+		border-bottom: dotted 1px #d2d2d2;
+	}
+	
+	.info-content span.col-change {
+		color: #409EFF;
+		cursor: pointer;
+	}
+	
+	.el-icon-circle-check {
+		position: relative;
+		top: 2px;
+		left: 10px;
+		color: #67C23A;
+	}
+	
+	.change-input {
+		width: 80%;
+		border-radius: 5px;
+		border: solid 1px #ccc;
+		padding: 10px;
+	}
 </style>
