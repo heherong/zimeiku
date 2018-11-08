7d8e42582220d846aa9b9bf7fe1cd8dab71402df
littleforest <670177821@qq.com>
2018-11-08 17:34:38 +0800

    去掉了网址的#号

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import ContentBank from '@/pages/contentBank/main/ContentBank'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -28,7 +29,7 @@ export default new Router({
     {
       path: '/market',
       name: 'market',
-      component: Market
+      component: Market,
     },
     {
       path: '/user',
